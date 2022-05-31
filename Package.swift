38ebaca0f2cf75442e18eae9b9ffc5f346bd042d
Sven A. Schmidt <sas@finestructure.co>
2022-05-31 17:58:07 +0200

    List ref and product for a given top level directory

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.1.2"),
         .package(url: "https://github.com/soto-project/soto.git", from: "5.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-parsing.git", from: "0.9.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -16,6 +17,7 @@ let package = Package(
             name: "s3-check",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Parsing", package: "swift-parsing"),
                 .product(name: "SotoS3", package: "soto"),
             ]),
         .testTarget(
--- Sources/s3-check/S3Check.swift
+++ Sources/s3-check/S3Check.swift
@@ -1,7 +1,107 @@
+import Foundation
+
 import ArgumentParser
+import Parsing
+import SotoS3
 
 
 @main
 struct S3Check: AsyncParsableCommand {
+    @Option(help: "AWS access key ID")
+    var awsAccessKeyId: String
 
+    @Option(help: "AWS bucket name")
+    var awsBucketName: String
+
+    @Option(help: "AWS secrete access key")
+    var awsSecretAccessKey: String
+
+    func run() async throws {
+        print("Checking \(awsBucketName)")
+
+        let key = S3StoreKey(bucket: awsBucketName, directory: "apple/swift-docc")
+        let client = AWSClient(credentialProvider: .static(accessKeyId: awsAccessKeyId,
+                                                           secretAccessKey: awsSecretAccessKey),
+                               httpClientProvider: .createNew)
+        defer { try? client.syncShutdown() }
+
+        let s3 = S3(client: client, region: .useast2)
+
+        // filter this down somewhat by eliminating `.json` files
+        let files = try await s3.listFiles(in: awsBucketName, key: key, delimiter: ".json")
+        for f in files {
+            if let path = try? folder.parse(f.file.key) {
+                print(path.ref, path.product)
+            }
+        }
+        print("Count: \(files.count)")
+    }
+
+}
+
+
+struct Path {
+    var owner: String
+    var repository: String
+    var ref: String
+    var product: String
+}
+
+
+let pathSegment = Parse {
+    PrefixUpTo("/").map(.string)
+    "/"
+}
+
+let folder = Parse(Path.init) {
+    pathSegment
+    pathSegment
+    pathSegment
+    "documentation/"
+    pathSegment
+    "index.html"
+}
+
+struct S3File {
+    var bucket: String
+    var key: String
+}
+
+
+struct S3FileDescriptor {
+    let file: S3File
+    let modificationDate: Date
+    let size: Int
+}
+
+extension S3 {
+    func listFiles(in bucket: String, key: S3StoreKey, delimiter: String? = nil) async throws -> [S3FileDescriptor] {
+        try await listFiles(in: bucket, key: key, delimiter: delimiter).get()
+    }
+
+    func listFiles(in bucket: String, key: S3StoreKey, delimiter: String? = nil) -> EventLoopFuture<[S3FileDescriptor]> {
+        let request = S3.ListObjectsV2Request(bucket: bucket, delimiter: delimiter, prefix: key.filename)
+        return self.listObjectsV2Paginator(request, []) { accumulator, response, eventLoop in
+            let files: [S3FileDescriptor] = response.contents?.compactMap {
+                guard let key = $0.key,
+                      let lastModified = $0.lastModified,
+                      let fileSize = $0.size else { return nil }
+                return S3FileDescriptor(
+                    file: S3File(bucket: bucket, key: key),
+                    modificationDate: lastModified,
+                    size: Int(fileSize)
+                )
+            } ?? []
+            return eventLoop.makeSucceededFuture((true, accumulator + files))
+        }
+    }
+}
+
+
+struct S3StoreKey {
+    let bucket: String
+    let directory: String
+
+    var filename: String { directory }
+    var url: String { "s3://\(bucket)/\(filename)" }
 }
