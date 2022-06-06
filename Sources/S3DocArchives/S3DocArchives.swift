c4776be756d6ab38ba86eae28e283c63a514ec89
Sven A. Schmidt <sas@finestructure.co>
2022-06-06 13:55:30 +0200

    Check for archive title

--- Sources/S3DocArchives/S3DocArchives.swift
+++ Sources/S3DocArchives/S3DocArchives.swift
@@ -5,55 +5,100 @@ import SotoS3
 
 public enum S3DocArchives {
 
-    public static func fetch(prefix: String,
-                             awsBucketName: String,
-                             awsAccessKeyId: String,
-                             awsSecretAccessKey: String) async throws -> [DocArchive] {
-        let key = S3.StoreKey(bucket: awsBucketName, directory: prefix)
-        let client = AWSClient(credentialProvider: .static(accessKeyId: awsAccessKeyId,
-                                                           secretAccessKey: awsSecretAccessKey),
-                               httpClientProvider: .createNew)
-        defer { try? client.syncShutdown() }
+    public static func fetch(
+        prefix: String,
+        awsBucketName: String,
+        awsAccessKeyId: String,
+        awsSecretAccessKey: String) async throws -> [DocArchive] {
 
-        let s3 = S3(client: client, region: .useast2)
+            let key = S3.StoreKey(bucket: awsBucketName, path: prefix)
+            let client = AWSClient(credentialProvider: .static(accessKeyId: awsAccessKeyId,
+                                                               secretAccessKey: awsSecretAccessKey),
+                                   httpClientProvider: .createNew)
+            defer { try? client.syncShutdown() }
 
-        // filter this down somewhat by eliminating `.json` files
-        return try await s3.listFiles(in: awsBucketName, key: key, delimiter: ".json")
-            .compactMap { try? folder.parse($0.file.key) }
-    }
+            let s3 = S3(client: client, region: .useast2)
+
+            // filter this down somewhat by eliminating `.json` files
+            let paths = try await s3.listFiles(key: key, delimiter: ".json")
+                .compactMap { try? DocArchive.path.parse($0.file.key) }
+
+            var archives = [DocArchive]()
+            for path in paths {
+                archives.append(await DocArchive(s3: s3, in: awsBucketName, path: path))
+            }
+
+            return archives
+        }
 
 
     public struct DocArchive: CustomStringConvertible {
-        var owner: String
-        var repository: String
-        var ref: String
-        var product: String
+        var path: Path
+        var title: String
+
+        init(s3: S3, in bucket: String, path: Path) async {
+            self.path = path
+            let key = S3.StoreKey(bucket: bucket, path: path.s3path)
+            self.title = (try? await s3.getDocArchiveTitle(key: key)) ?? path.product
+        }
 
         public var description: String {
-            "\(owner)/\(repository) @ \(ref) - \(product)"
+            "\(path) - \(title)"
         }
+
+        struct Path: CustomStringConvertible {
+            var owner: String
+            var repository: String
+            var ref: String
+            var product: String
+
+            var s3path: String { "\(owner)/\(repository)/\(ref)" }
+
+            public var description: String {
+                "\(owner)/\(repository) @ \(ref) - \(product)"
+            }
+
+            func getTitle(s3: S3, in bucket: String) async throws -> String {
+                let key = S3.StoreKey(bucket: bucket, path: s3path)
+                if let data = try await s3.getFileContent(key: key) {
+                    return try JSONDecoder().decode(DocumentationData.self, from: data)
+                        .metadata.title
+                } else {
+                    return product
+                }
+            }
+        }
+
+        static let pathSegment = Parse {
+            PrefixUpTo("/").map(.string)
+            "/"
+        }
+
+
+        static let path = Parse(DocArchive.Path.init) {
+            pathSegment
+            pathSegment
+            pathSegment
+            "documentation/"
+            pathSegment
+            "index.html"
+        }
+
     }
 
+    struct DocumentationData: Codable, Equatable {
+        var metadata: Metadata
 
-    static let pathSegment = Parse {
-        PrefixUpTo("/").map(.string)
-        "/"
-    }
-
-
-    static let folder = Parse(DocArchive.init) {
-        pathSegment
-        pathSegment
-        pathSegment
-        "documentation/"
-        pathSegment
-        "index.html"
+        struct Metadata: Codable, Equatable {
+                var title: String
+        }
     }
 
 }
 
 
 private extension S3 {
+
     struct File {
         var bucket: String
         var key: String
@@ -69,20 +114,25 @@ private extension S3 {
 
     struct StoreKey {
         let bucket: String
-        let directory: String
+        var path: String
 
-        var filename: String { directory }
-        var url: String { "s3://\(bucket)/\(filename)" }
+        var url: String { "s3://\(bucket)/\(path)" }
     }
 
-
-    func listFiles(in bucket: String, key: StoreKey, delimiter: String? = nil) async throws -> [FileDescriptor] {
-        try await listFiles(in: bucket, key: key, delimiter: delimiter).get()
+    func getFileContent(key: StoreKey) async throws -> Data? {
+        let getObjectRequest = S3.GetObjectRequest(bucket: key.bucket, key: key.path)
+        return try await self.getObject(getObjectRequest)
+            .body?.asData()
     }
 
-    func listFiles(in bucket: String, key: StoreKey, delimiter: String? = nil) -> EventLoopFuture<[FileDescriptor]> {
-        let request = S3.ListObjectsV2Request(bucket: bucket, delimiter: delimiter, prefix: key.filename)
-        return self.listObjectsV2Paginator(request, []) { accumulator, response, eventLoop in
+    func listFiles(key: StoreKey, delimiter: String? = nil) async throws -> [FileDescriptor] {
+        try await listFiles(key: key, delimiter: delimiter).get()
+    }
+
+    func listFiles(key: StoreKey, delimiter: String? = nil) -> EventLoopFuture<[FileDescriptor]> {
+        let bucket = key.bucket
+        let request = S3.ListObjectsV2Request(bucket: bucket, delimiter: delimiter, prefix: key.path)
+        return listObjectsV2Paginator(request, []) { accumulator, response, eventLoop in
             let files: [FileDescriptor] = response.contents?.compactMap {
                 guard let key = $0.key,
                       let lastModified = $0.lastModified,
@@ -96,6 +146,13 @@ private extension S3 {
             return eventLoop.makeSucceededFuture((true, accumulator + files))
         }
     }
+
+    func getDocArchiveTitle(key: StoreKey) async throws -> String? {
+        guard let data = try await getFileContent(key: key) else { return nil }
+        return try JSONDecoder().decode(S3DocArchives.DocumentationData.self, from: data)
+            .metadata.title
+    }
+
 }
 
 
